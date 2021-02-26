7d22b80447cd91908b1078af6b3e43cbf3043ccf
cam17380 <cam17380@gmail.com>
2021-02-26 17:16:57 +0900

    Chg. Local => Global state management.

---
+++ src/components/TodoForm.js
@@ -0,0 +1,28 @@
+import React, {useState, useContext} from 'react'
+import { Button, Form, Input, InputGroup, InputGroupAddon } from 'reactstrap';
+import { TodosContext } from '../contexts/TodosContext'
+
+function TodoForm() {
+    const { addTodo } = useContext(TodosContext)
+    const [value, setValue] = useState('')
+    const handleSubmit = e => {
+        e.preventDefault()
+        addTodo(value)
+        setValue('')
+      }
+    return (
+        <Form onSubmit={handleSubmit}>
+            <InputGroup>
+                <Input type="text"
+                    value={value}
+                    onChange={e => setValue(e.target.value)} />
+                <InputGroupAddon addonType="append">
+                    <Button type="submit" color="primary">追加</Button>
+                </InputGroupAddon>
+            </InputGroup>
+        </Form>
+
+    )
+}
+
+export default TodoForm
